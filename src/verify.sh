497e9e67fae81035045ea27a58e1f556864c5769
Samuel Chassot <samuel.chassot@epfl.ch>
2023-05-09 14:58:22 +0200

    scala-dotty to verify

--- src/verify.sh
+++ src/verify.sh
@@ -1,1 +1,1 @@
-stainless-scalac VerifiedRegex.scala  ListUtils.scala VerifiedLexer.scala -Dparallel=3 --config-file=stainless.conf
+stainless-dotty VerifiedRegex.scala ListUtils.scala VerifiedLexer.scala -Dparallel=3 --config-file=stainless.conf
